5af97802d2d1ebd8dcfb7898d41d8bea99072691
liger <289032179@qq.com>
2018-08-19 14:51:07 +0800

    动态页

---
+++ app/src/main/java/com/koala/diycat/main/adapter/DynamicAdapter.java
@@ -0,0 +1,137 @@
+package com.koala.diycat.main.adapter;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.koala.diycat.R;
+import com.koala.diycat.model.statuses.Status;
+import com.koala.diycat.utils.glide.GlideApp;
+import com.koala.diycat.utils.glide.GlideRoundTransform;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * @author Liger
+ * @date 2018/5/29 23:12
+ */
+public class DynamicAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private Context mContext;
+    private List<Status> mList;
+
+    private static final int TYPE_NORMAL = 1;
+    private static final int TYPE_END = 2;
+
+
+    public DynamicAdapter(Context context, List<Status> list) {
+        super();
+        mContext = context;
+        mList = list;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == TYPE_NORMAL) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.fragment_home_rv_item_timeline, parent, false);
+            return new MyViewHolder(view);
+        } else if (viewType == TYPE_END) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_end, parent, false);
+            return new EndHolder(view);
+        }
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof MyViewHolder) {
+            MyViewHolder myViewHolder = (MyViewHolder) holder;
+            Status status = mList.get(position);
+            myViewHolder.userNameTv.setText(status.getUser().getName());
+            myViewHolder.createTimeTv.setText("20小时前");
+//            holder.createTimeTv.setText(status.getCreatedAt());
+//            holder.sourceTv.setText(status.getSource());
+            myViewHolder.contentTv.setText(status.getText());
+
+            GlideApp.with(mContext)
+                    .load(status.getUser().getProfile_image_url())
+                    .centerCrop()
+                    .transform(new GlideRoundTransform(25))
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            return false;
+                        }
+                    })
+                    .into(myViewHolder.userIv);
+        } else if (holder instanceof EndHolder) {
+            Log.d("liger", "onBindViewHolder: EndHolder");
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList == null ? 0 : mList.size() + 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Log.d("liger", "getItemViewType: position = " + position);
+        if (position == mList.size()) {
+            Log.d("liger", "getItemViewType: TYPE_END = " + TYPE_END);
+            return TYPE_END;
+        } else {
+            Log.d("liger", "getItemViewType: TYPE_NORMAL = " + TYPE_NORMAL);
+            return TYPE_NORMAL;
+        }
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.timeline_head_iv)
+        ImageView userIv;
+        @BindView(R.id.timeline_head_name_tv)
+        TextView userNameTv;
+        @BindView(R.id.timeline_head_time_tv)
+        TextView createTimeTv;
+        @BindView(R.id.timeline_head_source_tv)
+        TextView sourceTv;
+
+        @BindView(R.id.item_timeline_content_tv)
+        TextView contentTv;
+
+        MyViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    class EndHolder extends RecyclerView.ViewHolder {
+
+        EndHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
--- app/src/main/java/com/koala/diycat/main/home/DynamicFragment.java
+++ app/src/main/java/com/koala/diycat/main/home/DynamicFragment.java
@@ -1,6 +1,27 @@
 package com.koala.diycat.main.home;
 
+import android.annotation.SuppressLint;
+import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
+
+import com.koala.diycat.api.ApiService;
+import com.koala.diycat.api.Uri;
 import com.koala.diycat.base.BaseRefreshFragment;
+import com.koala.diycat.main.adapter.DynamicAdapter;
+import com.koala.diycat.model.statuses.Status;
+import com.koala.diycat.model.statuses.TimeLine;
+import com.koala.diycat.network.NetManager;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * @author Liger
@@ -8,14 +29,96 @@ import com.koala.diycat.base.BaseRefreshFragment;
  */
 public class DynamicFragment extends BaseRefreshFragment {
 
+    private List<Status> mDatas;
+    private DynamicAdapter mAdapter;
+    private int page = 1;
+    Map<String, Integer> options;
+
     @Override
     protected void initView() {
         super.initView();
+        mDatas = new ArrayList<>();
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+        mAdapter = new DynamicAdapter(mContext,mDatas);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @SuppressLint("CheckResult")
+    @Override
+    protected void initData() {
+        getHomeTimeLine(page);
+    }
+
+    private void getHomeTimeLine(int pageCount) {
+        options = new HashMap<>(1);
+        options.put(Uri.PAGE, pageCount);
+        NetManager.getInstance().retrofit().create(ApiService.class).getHomeTimeLine(options)
+                //在 io 线程请求网络
+                .subscribeOn(Schedulers.io())
+                //回到主线程处理请求结果
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<TimeLine>() {
+                    private Disposable mDisposable;
+
+                    /**
+                     * 发送请求后调用该方法
+                     * @param d
+                     */
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        mDisposable = d;
+                    }
+
+                    /**
+                     * 发送请求成功后,调用 onNext(),onComplete()方法
+                     * @param timeLine
+                     */
+                    @Override
+                    public void onNext(TimeLine timeLine) {
+                        List<Status> statuses = timeLine.getStatuses();
+                        if (statuses == null || statuses.size() <= 0) {
+                            return;
+                        }
+                        if (page == 1) {
+                            mDatas.clear();
+                            mDatas.addAll(timeLine.getStatuses());
+                            mAdapter.notifyDataSetChanged();
+                        } else {
+                            List<Status> list = new ArrayList<>(timeLine.getStatuses());
+                            int position = mDatas.size();
+                            mDatas.addAll(position, list);
+                            mAdapter.notifyItemInserted(position);
+                        }
+                        page++;
+                    }
+
+                    /**
+                     * 发送请求失败调用该方法
+                     * @param e
+                     */
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d("liger", "onError: 请求失败" + e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d("liger", "onComplete: 请求成功");
+                    }
+                });
     }
 
     @Override
-    protected void initData() {
+    public void onRefresh(RefreshLayout refreshLayout) {
+        super.onRefresh(refreshLayout);
+        page = 1;
+//        getHomeTimeLine(page);
+    }
 
+    @Override
+    public void onLoadMore(RefreshLayout refreshLayout) {
+        super.onLoadMore(refreshLayout);
+//        getHomeTimeLine(page);
     }
 
 }
